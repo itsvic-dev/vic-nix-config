3d440d064352aa2b353cd8a857659077da6a9c8c
Victor B. <contact@itsvic.dev>
2024-04-27 21:44:38 +0200

    move tofi config to HM

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -10,10 +10,19 @@
     pkgs.pcmanfm-qt
   ];
 
-  home.file = {
-    ".config/tofi/config" = {
-      text = builtins.readFile ./config/tofi;
-      executable = false;
+  programs.tofi = {
+    enable = true;
+    settings = {
+      width = "100%";
+      height = "100%";
+      border-width = 0;
+      outline-width = 0;
+      padding-left = "35%";
+      padding-top = "35%";
+      result-spacing = 25;
+      num-results = 5;
+      font = "monospace";
+      background-color = "#000A";
     };
   };
 
