81b079b7580261ac277f0f9d5696cfbb42091e29
Victor B. <contact@itsvic.dev>
2024-04-28 09:18:44 +0200

    move ime settings to HM

--- home/default.nix
+++ home/default.nix
@@ -1,12 +1,11 @@
 { config, pkgs, ... }:
 {
   home.stateVersion = "23.11";
-  home.packages = with pkgs; [
-    nix-output-monitor
-  ];
+  home.packages = with pkgs; [ nix-output-monitor ];
 
   imports = [
     ./hyprland.nix
     ./gtk.nix
+    ./ime.nix
   ];
 }
--- machines/e6nix.nix
+++ machines/e6nix.nix
@@ -71,14 +71,6 @@
     LC_TIME = "ja_JP.UTF-8";
   };
 
-  i18n.inputMethod = {
-    enabled = "fcitx5";
-    fcitx5.addons = with pkgs; [
-      fcitx5-mozc
-      fcitx5-gtk
-    ];
-  };
-
   # Configure console keymap
   console.keyMap = "pl2";
 
