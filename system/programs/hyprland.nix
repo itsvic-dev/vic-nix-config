b8f59806cb6b141634d0ef3e0a432122341ffaa2
Victor B. <contact@itsvic.dev>
2024-05-11 08:54:57 +0200

    remove hyprland, switch to kde

--- system/programs/hyprland.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, inputs, ... }:
-{
-  nixpkgs.overlays = [
-    (final: prev: { hyprland = pkgs.callPackage ../../packages/hyprland.nix { inherit inputs; }; })
-  ];
-  programs.hyprland = {
-    enable = true;
-  };
-}
--- system/services/xserver.nix
+++ system/services/xserver.nix
@@ -2,6 +2,7 @@
   services.xserver = {
     enable = true;
     displayManager.sddm.enable = true;
+    desktopManager.plasma6.enable = true;
     libinput.enable = true;
   };
 }
