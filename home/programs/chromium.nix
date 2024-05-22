dff9c2a57cd2e60fdd7a2af37ecc08a4dc5cdc6f
Victor B. <contact@itsvic.dev>
2024-05-22 20:56:36 +0200

    enable widevine

--- home/programs/chromium.nix
+++ home/programs/chromium.nix
@@ -1,6 +1,8 @@
+{ pkgs, ... }:
 {
   programs.chromium = {
     enable = true;
+    package = pkgs.chromium.override { enableWideVine = true; };
     extensions = [
       { id = "nngceckbapebfimnlniiiahkandclblb"; } # Bitwarden
       { id = "eimadpbcbfnmbkopoojfekhnkhdbieeh"; } # Dark Reader
