eeb2aa639bc24ff25925885cd1d0eb4c30b57c7c
Victor B. <contact@itsvic.dev>
2024-06-04 18:35:21 +0200

    reconfigure tastypi

--- system/machines/tastypi.nix
+++ system/machines/tastypi.nix
@@ -9,23 +9,28 @@
 {
   imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
+  vic-nix = {
+    desktop.enable = true;
+    hardware.bluetooth = true;
+  };
+
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ ];
   boot.kernelPackages = pkgs.linuxPackages_rpi4;
   boot.extraModulePackages = [ ];
 
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/de43a149-9439-4560-b656-270b8ecf7717";
-    fsType = "btrfs";
+    device = "/dev/nvme0n1p2";
+    fsType = "ext4";
   };
 
   fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/160B-83C7";
+    device = "/dev/nvme0n1p1";
     fsType = "vfat";
   };
   swapDevices = [ ];
   networking.useDHCP = lib.mkDefault true;
 
   # needed by... vscode extensions??? wtf?
-  nixpkgs.config.allowUnsupportedSystem = true;
+  # nixpkgs.config.allowUnsupportedSystem = true;
 }
