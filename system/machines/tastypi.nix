74bce21b9ac8611e878202d53b587a2f52118f3f
Victor B. <contact@itsvic.dev>
2024-06-05 12:32:25 +0200

    remove akos ipv6 for now (slow as balls)

--- system/machines/tastypi.nix
+++ system/machines/tastypi.nix
@@ -12,24 +12,6 @@
     hardware.bluetooth = true;
   };
 
-  sops.secrets.akos-ipv6-pk.sopsFile = ../../secrets/tastypi.yaml;
-
-  networking.wireguard.interfaces = {
-    akos-ipv6 = {
-      ips = [ "2a0e:97c0:7c5::2/48" ];
-      listenPort = 51820;
-      privateKeyFile = config.sops.secrets.akos-ipv6-pk.path;
-      peers = [
-        {
-          publicKey = "z0FLHeJGlkRlp+e5WXJuQz2O3xSCJs74s++4dWDlZ1s=";
-          allowedIPs = [ "::/0" ];
-          endpoint = "45.136.137.31:999";
-          persistentKeepalive = 25;
-        }
-      ];
-    };
-  };
-
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ ];
   boot.kernelPackages = pkgs.linuxPackages_rpi4;
