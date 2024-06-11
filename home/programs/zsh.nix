08d1cc022e92885d4e7f908e8e525107d6919f81
Victor B. <contact@itsvic.dev>
2024-06-11 22:15:46 +0200

    time to fix bp

--- home/programs/zsh.nix
+++ home/programs/zsh.nix
@@ -17,9 +17,11 @@
         VERSION=dev-vic
         sudo rsync -av . /var/www/pterodactyl/ --chown nginx:nginx --exclude '/blueprint' --exclude '/routes' --include '/resources/views/blueprint'
         sudo sed -E -i "s*::v*$VERSION*g" $FOLDER/app/BlueprintFramework/Services/PlaceholderService/BlueprintPlaceholderService.php
+        sudo sed -E -i "s*::v*$VERSION*g" $FOLDER/.blueprint/extensions/blueprint/public/index.html
         sudo sed -i "s~::f~$FOLDER~g" $FOLDER/app/BlueprintFramework/Services/PlaceholderService/BlueprintPlaceholderService.php
         sudo sed -i "s~::f~$FOLDER~g" $FOLDER/app/BlueprintFramework/Libraries/ExtensionLibrary/Admin/BlueprintAdminLibrary.php
         sudo sed -i "s~::f~$FOLDER~g" $FOLDER/app/BlueprintFramework/Libraries/ExtensionLibrary/Client/BlueprintClientLibrary.php
+        sudo cp $FOLDER/.blueprint/assets/logo.jpg $FOLDER/.blueprint/extensions/blueprint/assets/logo.jpg
         sudo sed -i "s/NOTINSTALLED/INSTALLED/g" $FOLDER/app/BlueprintFramework/Services/PlaceholderService/BlueprintPlaceholderService.php
       }
     '';
