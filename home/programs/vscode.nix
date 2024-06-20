ec13ea6462db547f6eaa23dde34738154c32bf5f
Victor B. <contact@itsvic.dev>
2024-06-20 16:19:04 +0200

    vscode stuffs

--- home/programs/vscode.nix
+++ home/programs/vscode.nix
@@ -12,16 +12,50 @@ in
     stylix.targets.vscode.enable = false; # the stylix theme for vscode sucks balls
     programs.vscode = {
       enable = true;
+      enableUpdateCheck = false;
+
       extensions = with pkgs.vscode-extensions; [
+        # Python
+        ms-python.python
+        ms-python.black-formatter
+        ms-python.vscode-pylance
+
+        # C/C++
+        llvm-vs-code-extensions.vscode-clangd
+        twxs.cmake
+        ms-vscode.cmake-tools
+
+        # Nix
         bbenoist.nix
         brettm12345.nixfmt-vscode
-        ms-python.python
-        llvm-vs-code-extensions.vscode-clangd
-        vue.volar
+
+        # Node.js/Web
         dbaeumer.vscode-eslint
-        bradlc.vscode-tailwindcss
+        vunguyentuan.vscode-postcss
         esbenp.prettier-vscode
+        vue.volar
+        bradlc.vscode-tailwindcss
       ];
+
+      userSettings =
+        let
+          prettier = {
+            "editor.defaultFormatter" = "esbenp.prettier-vscode";
+          };
+        in
+        {
+          "editor.tabSize" = 2;
+          "editor.formatOnSave" = true;
+
+          "[vue]" = prettier;
+          "[typescript]" = prettier;
+          "[typescriptreact]" = prettier;
+
+          "git.confirmSync" = false;
+          "git.enableSmartCommit" = true;
+
+          "php.validate.executablePath" = "${pkgs.php}/bin/php";
+        };
     };
   };
 }
