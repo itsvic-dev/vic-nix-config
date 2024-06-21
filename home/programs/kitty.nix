f2c523887b034107931dd0304f49c023401ffc8e
Victor B. <contact@itsvic.dev>
2024-06-21 16:15:34 +0200

    kitty: add padding

--- home/programs/kitty.nix
+++ home/programs/kitty.nix
@@ -1,5 +1,19 @@
 {
-  programs.kitty = {
-    enable = true;
+  nixosConfig,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = nixosConfig.vic-nix.desktop;
+in
+{
+  config = lib.mkIf cfg.enable {
+    programs.kitty = {
+      enable = true;
+      settings = {
+        window_padding_width = 8;
+      };
+    };
   };
 }
