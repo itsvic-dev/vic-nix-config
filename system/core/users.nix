c6550f98dfaa604e1a7c0d329e0dd6daa620ff65
Victor B. <contact@itsvic.dev>
2024-07-22 11:11:44 +0200

    add ssh key

--- system/core/users.nix
+++ system/core/users.nix
@@ -22,6 +22,7 @@
 
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPQ9kubQPsQKJdtOLG35DApD9IWNU5gR6Dm+e2cy2L83 vic@e6nix"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICwzZPUNnSF9PNKp4RlYL8xggd52nE139qFAKQdpPKYf" # Termius on Pixel 8
     ];
   };
 
