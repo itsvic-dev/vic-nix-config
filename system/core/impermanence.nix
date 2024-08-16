592c7edae60ef6640e3c5c3e931cb6f8e4dcf9ff
Victor B. <contact@itsvic.dev>
2024-08-16 15:54:39 +0200

    libvirt impermanence

--- system/core/impermanence.nix
+++ system/core/impermanence.nix
@@ -21,6 +21,7 @@
         ++ lib.optionals config.vic-nix.hardware.bluetooth [ "/var/lib/bluetooth" ]
         ++ lib.optionals config.vic-nix.secureBoot [ "/etc/secureboot" ] # sbctl keys
         ++ lib.optionals config.services.accounts-daemon.enable [ "/var/lib/AccountsService" ] # pfp and last session data
+        ++ lib.optionals config.vic-nix.software.libvirt [ "/var/lib/libvirt" ];
       ;
 
       files = [ "/etc/machine-id" ];
