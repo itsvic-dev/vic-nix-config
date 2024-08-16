90ddc0bc4538553846cbe4b66db89fc382947b1f
Victor B. <contact@itsvic.dev>
2024-08-16 17:23:04 +0200

    delete testlab lol

--- system/machines/tastypi/networking.nix
+++ system/machines/tastypi/networking.nix
@@ -2,7 +2,6 @@
 {
   sops.secrets = {
     akos-ipv6-pk.sopsFile = ../../../secrets/tastypi.yaml;
-    testlab-pk.sopsFile = ../../../secrets/tastypi.yaml;
   };
 
   networking.wireguard.interfaces = {
@@ -19,43 +18,7 @@
         }
       ];
     };
-
-    testlab = {
-      ips = [ "2a0e:97c0:7c5::3/128" ];
-      listenPort = 51821;
-      privateKeyFile = config.sops.secrets.testlab-pk.path;
-
-      postSetup = ''
-        ${pkgs.iptables}/bin/ip6tables -A FORWARD -i testlab -o akos-ipv6 -j ACCEPT
-        ${pkgs.iptables}/bin/ip6tables -A FORWARD -i akos-ipv6 -o testlab -j ACCEPT
-        ${pkgs.iproute2}/bin/ip -6 route add 2a0e:97c0:7c5:e621::/64 via 2a0e:97c0:7c5::4
-      '';
-
-      postShutdown = ''
-        ${pkgs.iptables}/bin/ip6tables -D FORWARD -i testlab -o akos-ipv6 -j ACCEPT
-        ${pkgs.iptables}/bin/ip6tables -D FORWARD -i akos-ipv6 -o testlab -j ACCEPT
-        ${pkgs.iproute2}/bin/ip -6 route del 2a0e:97c0:7c5:e621::/64 via 2a0e:97c0:7c5::4
-      '';
-
-      peers = [
-        {
-          publicKey = "s+g8KhlC/hL/E51v1Sn++Ggm0qiHuQ/NIRZb3s2ZnBo=";
-          allowedIPs = [
-            "2a0e:97c0:7c5:e621::/64"
-            "2a0e:97c0:7c5::4/128"
-          ];
-        }
-      ];
-    };
   };
-
-  boot.kernel.sysctl = {
-    # If you want to use it for ipv6
-    "net.ipv6.conf.all.forwarding" = true;
-    "net.ipv6.conf.default.forwarding" = true;
-  };
-
-  networking.firewall.allowedUDPPorts = [ 51821 ];
 
   environment.etc."gai.conf".text = ''
     label  ::1/128       0
