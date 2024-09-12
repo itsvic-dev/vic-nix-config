071e73c443527823d97db9253d8770a7e3532c20
Victor B. <contact@itsvic.dev>
2024-09-12 14:47:50 +0200

    docker: impermanence

--- system/misc/docker.nix
+++ system/misc/docker.nix
@@ -1,7 +1,14 @@
 { config, lib, ... }:
 {
-  config = lib.mkIf config.vic-nix.software.docker {
-    virtualisation.docker.enable = true;
-    users.users.vic.extraGroups = [ "docker" ];
-  };
+  config = lib.mkIf config.vic-nix.software.docker (
+    lib.mkMerge [
+      {
+        virtualisation.docker.enable = true;
+        users.users.vic.extraGroups = [ "docker" ];
+      }
+      (lib.mkIf config.vic-nix.tmpfsAsRoot {
+        environment.persistence."/persist".directories = [ "/var/lib/docker" ];
+      })
+    ]
+  );
 }
