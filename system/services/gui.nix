e6d4b40c4f79fbb18a34797d547419a8c7441b7a
Victor B. <contact@itsvic.dev>
2024-10-03 15:11:37 +0200

    epic fail

--- system/services/gui.nix
+++ system/services/gui.nix
@@ -9,32 +9,31 @@ let
 in
 {
   config = lib.mkIf cfg.enable (
-    lib.mkMerge
-      [
-        {
-          services = {
-            xserver.enable = true;
-            libinput.enable = true;
+    lib.mkMerge [
+      {
+        services = {
+          xserver.enable = true;
+          libinput.enable = true;
 
-            # needed for some file management stuff
-            gvfs.enable = true;
-          };
+          # needed for some file management stuff
+          gvfs.enable = true;
+        };
 
-          # makes gstreamer work properly
-          environment.sessionVariables.GST_PLUGIN_SYSTEM_PATH_1_0 =
-            lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0"
-              [
-                pkgs.gst_all_1.gst-plugins-good
-                pkgs.gst_all_1.gst-plugins-bad
-                pkgs.gst_all_1.gst-plugins-ugly
-              ];
-        }
-      ]
+        # makes gstreamer work properly
+        environment.sessionVariables.GST_PLUGIN_SYSTEM_PATH_1_0 =
+          lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0"
+            [
+              pkgs.gst_all_1.gst-plugins-good
+              pkgs.gst_all_1.gst-plugins-bad
+              pkgs.gst_all_1.gst-plugins-ugly
+            ];
+      }
       (
         lib.mkIf cfg.environment == "gnome" {
           services.displayManager.gdm.enable = true;
           services.desktopManager.gnome.enable = true;
         }
       )
+    ]
   );
 }
