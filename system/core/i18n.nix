c551c8905c69fd18e0208e809a89ca583c9d289d
Victor B. <contact@itsvic.dev>
2024-11-08 11:29:07 +0100

    hi keychron

--- system/core/i18n.nix
+++ system/core/i18n.nix
@@ -7,10 +7,4 @@
       LC_TIME = "pl_PL.UTF-8";
     };
   };
-
-  # NOTE: idk how well this will play with KMonad in userspace,
-  # i just want it working in initrd for now.
-  # i hardly use the TTY on desktop anyway, and I can just bring
-  # the keyboard out of NKRO mode so KMonad doesn't affect it.
-  console.keyMap = if (config.services.kmonad.enable == true) then "mod-dh-ansi-us" else "pl2";
 }
