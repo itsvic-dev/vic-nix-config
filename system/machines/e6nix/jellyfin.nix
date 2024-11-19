9f7f8d1575464f182b4bb1263495d71dadd0b8f8
Victor B. <contact@itsvic.dev>
2024-11-19 19:40:52 +0100

    what the FUCK is a "firewall"

--- system/machines/e6nix/jellyfin.nix
+++ system/machines/e6nix/jellyfin.nix
@@ -8,13 +8,42 @@
 
   services = {
     jellyfin.enable = true;
+
     sonarr.enable = true;
     radarr.enable = true;
     bazarr.enable = false; # not needed for now tbh
     prowlarr.enable = true;
+
     aria2 = {
       enable = true;
       rpcSecretFile = config.sops.secrets.aria2-rpc-secret.path;
+    };
+
+    nginx = {
+      enable = true;
+      recommendedProxySettings = true;
+      recommendedTlsSettings = true;
+
+      virtualHosts."jellyfin.itsvic.dev" = {
+        enableACME = true;
+        forceSSL = true;
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:8096";
+          proxyWebsockets = true;
+        };
+      };
+    };
+  };
+
+  networking.firewall.allowedTCPPorts = [
+    80
+    443
+  ];
+
+  security.acme = {
+    acceptTerms = true;
+    defaults = {
+      email = "contact@itsvic.dev";
     };
   };
 
