bad7461b2935a8cd5e71fa7356f8c68ffc0ba064
Victor B. <contact@itsvic.dev>
2025-01-14 09:32:18 +0100

    Revert "libvirt: autostart default network"
    This reverts commit e6f3ce1f504f15c1188f104eb1fc54558598d6ca.

--- system/programs/libvirt.nix
+++ system/programs/libvirt.nix
@@ -13,11 +13,8 @@ in
       {
         virtualisation.libvirtd = {
           enable = true;
-          qemu = {
-            swtpm.enable = true;
-            ovmf.packages = [ pkgs.OVMFFull.fd ];
-            autostartDefaultNetworks = true;
-          };
+          qemu.swtpm.enable = true;
+          qemu.ovmf.packages = [ pkgs.OVMFFull.fd ];
         };
 
         virtualisation.spiceUSBRedirection.enable = true;
