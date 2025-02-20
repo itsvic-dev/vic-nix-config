33598b0e51ec008d111a394ef6ae1cdd744cb3ea
Victor B. <contact@itsvic.dev>
2025-02-20 11:20:11 +0100

    hopefully fixes SR4 issues

--- system/programs/steam.nix
+++ system/programs/steam.nix
@@ -1,7 +1,18 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   cfg = config.vic-nix.desktop;
 in
 {
-  config = lib.mkIf (cfg.enable && cfg.forGaming) { programs.steam.enable = true; };
+  config = lib.mkIf (cfg.enable && cfg.forGaming) {
+    programs.steam = {
+      enable = true;
+      package = pkgs.steam.override ({ extraLibraries = p: with p; [ SDL2 ]; });
+      protontricks.enable = true;
+    };
+  };
 }
