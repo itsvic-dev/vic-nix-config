e115b0e46d3dda562aeac8d418e8c252b7dec20a
Victor B. <contact@itsvic.dev>
2025-03-18 15:37:49 +0100

    more nix-darwin fixes

--- darwin/core/default.nix
+++ darwin/core/default.nix
@@ -1,5 +1,15 @@
-{
+{ pkgs, config, lib, ... }: {
   imports = [ ../../system/core/nix.nix ];
 
   system.stateVersion = 6;
+
+  system.build.applications = lib.mkForce (pkgs.buildEnv {
+    name = "system-applications";
+    paths = config.environment.systemPackages
+      ++ config.home-manager.users.vic.home.packages;
+    pathsToLink = "/Applications";
+  });
+
+  # disable HM's default linking behaviour (does not play well with Launchpad or Spotlight)
+  home-manager.users.vic.targets.darwin.linkApps.enable = false;
 }
--- home/programs/mpv.nix
+++ home/programs/mpv.nix
@@ -2,6 +2,7 @@
   config = lib.mkIf osConfig.vic-nix.desktop.enable {
     programs.mpv = {
       enable = true;
+      package = if (pkgs.stdenv.isDarwin) then pkgs.mpv-unwrapped else pkgs.mpv;
       config = {
         hwdec = "auto-safe";
         vo = "gpu";
