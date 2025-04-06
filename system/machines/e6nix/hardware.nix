865738818cacf4977194e4c92c13d0f1ceeb1e7b
Victor B. <contact@itsvic.dev>
2025-04-06 17:14:54 +0200

    e6nix: remove

--- system/machines/e6nix/hardware.nix
+++
@@ -1,9 +0,0 @@
-{ config, ... }: {
-  boot.extraModulePackages = with config.boot.kernelPackages; [ acpi_call ];
-  boot.initrd.availableKernelModules =
-    [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" "i915" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
-
-  powerManagement.cpuFreqGovernor = "powersave";
-}
