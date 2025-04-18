ec157a5bd585cfd5f62699412a6bba94febe068e
Victor B. <contact@itsvic.dev>
2025-04-18 22:37:12 +0200

    sfdbf

--- darwin/core/default.nix
+++ darwin/core/default.nix
@@ -12,4 +12,7 @@
 
   # disable HM's default linking behaviour (does not play well with Launchpad or Spotlight)
   home-manager.users.vic.targets.darwin.linkApps.enable = false;
+
+  # install ncurses (has better clear :P)
+  environment.systemPackages = with pkgs; [ ncurses ];
 }
