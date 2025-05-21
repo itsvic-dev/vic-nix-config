30f6c545942b17397b7b3baba9f78ed26ba468f2
Victor B. <contact@itsvic.dev>
2025-05-21 19:46:30 +0200

    fix™

--- system/pkgs/default.nix
+++ system/pkgs/default.nix
@@ -1,22 +1,24 @@
 final: prev: {
   # set of custom pkgs, either written or updated, that havent entered nixpkgs yet
-  libtorrent = prev.libtorrent.overrideAttrs (final: prev: rec {
-    version = "0.15.3";
-    src = fetchFromGitHub {
-      owner = "rakshasa";
-      repo = "libtorrent";
-      rev = "v${version}";
-      hash = "sha256-7WviN4KA236esXXCGKi+TiqRIg4hD+5EblTFqQRiRks=";
-    };
-  });
+  libtorrent = with prev;
+    prev.libtorrent.overrideAttrs (final: prev: rec {
+      version = "0.15.3";
+      src = fetchFromGitHub {
+        owner = "rakshasa";
+        repo = "libtorrent";
+        rev = "v${version}";
+        hash = "sha256-7WviN4KA236esXXCGKi+TiqRIg4hD+5EblTFqQRiRks=";
+      };
+    });
 
-  rtorrent = prev.rtorrent.overrideAttrs (final: prev: rec {
-    version = "0.15.3";
-    src = fetchFromGitHub {
-      owner = "rakshasa";
-      repo = "rtorrent";
-      rev = "v${version}";
-      hash = "";
-    };
-  });
+  rtorrent = with prev;
+    prev.rtorrent.overrideAttrs (final: prev: rec {
+      version = "0.15.3";
+      src = fetchFromGitHub {
+        owner = "rakshasa";
+        repo = "rtorrent";
+        rev = "v${version}";
+        hash = "sha256-etaigf7lx+WAf34NPN6KmsMB5jW4woA7f3VauIZNRfM=";
+      };
+    });
 }
