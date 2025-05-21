7b02e77752690c87bfddc44995218f6b830063af
Victor B. <contact@itsvic.dev>
2025-05-21 20:13:03 +0200

    tastypi: poo poo fart

--- system/machines/tastypi/free-media.nix
+++ system/machines/tastypi/free-media.nix
@@ -7,10 +7,49 @@
 
   services.rtorrent = {
     enable = true;
-    downloadDir = "/var/media";
+    downloadDir = "/var/torrents";
     openFirewall = true;
   };
 
-  # create /var/media folder
-  systemd.tmpfiles.rules = [ "d '/var/media' 0777 nobody nogroup -" ];
+  services.samba = {
+    enable = true;
+    securityType = "user";
+    openFirewall = true;
+    settings = {
+      global = {
+        workgroup = "WORKGROUP";
+        "server string" = "tastypi";
+        "netbios name" = "tastypi";
+        security = "user";
+        "hosts allow" = "192.168.0. 127.0.0.1 localhost";
+        "hosts deny" = "0.0.0.0/0";
+        "guest account" = "nobody";
+        "map to guest" = "bad user";
+      };
+
+      torrents = {
+        path = "/var/torrents";
+        browseable = "yes";
+        "read only" = "yes";
+        "guest ok" = "yes";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+      };
+
+      media = {
+        path = "/var/media";
+        browseable = "yes";
+        "read only" = "yes";
+        "guest ok" = "yes";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+      };
+    };
+  };
+
+  # create folders
+  systemd.tmpfiles.rules = [
+    "d '/var/torrents' 0777 nobody nogroup -"
+    "d '/var/media' 0777 nobody nogroup -"
+  ];
 }
