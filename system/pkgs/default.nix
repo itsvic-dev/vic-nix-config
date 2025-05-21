a97438e517201d8c95f72c19434c0e0de9518c95
Victor B. <contact@itsvic.dev>
2025-05-21 19:44:08 +0200

    update rtorrent

--- system/pkgs/default.nix
+++ system/pkgs/default.nix
@@ -1,4 +1,22 @@
 final: prev: {
   # set of custom pkgs, either written or updated, that havent entered nixpkgs yet
-  cider-2 = final.callPackage ./cider-2.nix { };
+  libtorrent = prev.libtorrent.overrideAttrs (final: prev: rec {
+    version = "0.15.3";
+    src = fetchFromGitHub {
+      owner = "rakshasa";
+      repo = "libtorrent";
+      rev = "v${version}";
+      hash = "sha256-7WviN4KA236esXXCGKi+TiqRIg4hD+5EblTFqQRiRks=";
+    };
+  });
+
+  rtorrent = prev.rtorrent.overrideAttrs (final: prev: rec {
+    version = "0.15.3";
+    src = fetchFromGitHub {
+      owner = "rakshasa";
+      repo = "rtorrent";
+      rev = "v${version}";
+      hash = "";
+    };
+  });
 }
