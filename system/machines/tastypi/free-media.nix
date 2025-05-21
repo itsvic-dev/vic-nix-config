00b011f21b7bd0f2ed41d23055b24da774ae2386
Victor B. <contact@itsvic.dev>
2025-05-21 19:39:13 +0200

    tastypi: rtorrent

--- system/machines/tastypi/free-media.nix
+++ system/machines/tastypi/free-media.nix
@@ -4,4 +4,12 @@
     host = "192.168.0.134";
     openFirewall = true;
   };
+
+  services.rtorrent = {
+    enable = true;
+    downloadDir = "/var/media";
+  };
+
+  # create /var/media folder
+  systemd.tmpfiles.rules = [ "d '/var/media' 0777 nobody nogroup -" ];
 }
