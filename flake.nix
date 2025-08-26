741f47ad97efc1fb18a84635725bf313d953eb47
Victor B. <contact@itsvic.dev>
2025-08-26 21:38:05 +0200

    flgkfl

--- flake.nix
+++ flake.nix
@@ -52,7 +52,7 @@
     bob = {
       url =
         "github:bob-discord-bot/bob/38845c889339281250e6189ba69480b1ab46e9c1";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgsStable";
     };
 
     # tastypi
