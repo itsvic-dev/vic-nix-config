5513897ced3c3631f3f5b34f3ac7c2b370e93e2a
Victor B. <contact@itsvic.dev>
2025-09-27 14:16:20 +0200

    mbp: add cargo-binutils

--- darwin/machines/mbp/default.nix
+++ darwin/machines/mbp/default.nix
@@ -10,7 +10,7 @@
 
   fonts.packages = with pkgs; [ nerd-fonts.jetbrains-mono ];
 
-  environment.systemPackages = with pkgs; [ nixfmt-classic gnupg rustup ];
+  environment.systemPackages = with pkgs; [ nixfmt-classic gnupg rustup cargo-binutils ];
 
   programs.gnupg.agent.enable = true;
 }
