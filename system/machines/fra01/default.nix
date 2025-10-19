a2e99c9a92d67a9903f80fbbfdb468736c8dc612
Victor B. <contact@itsvic.dev>
2025-10-20 00:04:54 +0200

    bleh

--- system/machines/fra01/default.nix
+++ system/machines/fra01/default.nix
@@ -1,9 +1,26 @@
-{
+{ lib, ... }: {
   imports = [ ./disko.nix ./hardware.nix ];
 
   vic-nix = {
     server.enable = true;
     noSecrets = true;
+  };
+
+  networking = {
+    useNetworkd = true;
+    networkmanager.enable = lib.mkForce false;
+
+    interfaces = {
+      ens18.ipv4.addresses = [{
+        address = "37.114.50.122";
+        prefixLength = 24;
+      }];
+    };
+
+    defaultGateway = {
+      address = "37.114.50.1";
+      interface = "ens18";
+    };
   };
 
   users.users.root.hashedPassword =
