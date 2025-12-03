52129fc5c532f53dc86b540f330ef9de4baab1a3
Victor B. <contact@itsvic.dev>
2025-12-03 19:27:32 +0100

    nodejs includes its own corepack now, we can remove

--- home/packages.nix
+++ home/packages.nix
@@ -1,23 +1,40 @@
-{ lib, osConfig, pkgs, ... }:
-let cfg = osConfig.vic-nix;
-in {
-  home.packages = with pkgs;
-    [ git _7zz nix-output-monitor wget ] ++ lib.optionals cfg.desktop.enable
-    ([ qbittorrent ] ++ lib.optionals pkgs.stdenv.isLinux [
-      pavucontrol
-      telegram-desktop
-      (vesktop.override { withSystemVencord = false; })
-      thunderbird
-      # (nixpkgs-gimp-master.legacyPackages.${pkgs.system}.gimp)
-      obs-studio
-      mission-center
-    ] ++ lib.optionals cfg.desktop.forGaming [ wineWowPackages.stagingFull ]
+{
+  lib,
+  osConfig,
+  pkgs,
+  ...
+}:
+let
+  cfg = osConfig.vic-nix;
+in
+{
+  home.packages =
+    with pkgs;
+    [
+      git
+      _7zz
+      nix-output-monitor
+      wget
+    ]
+    ++ lib.optionals cfg.desktop.enable (
+      [ qbittorrent ]
+      ++ lib.optionals pkgs.stdenv.isLinux [
+        pavucontrol
+        telegram-desktop
+        (vesktop.override { withSystemVencord = false; })
+        thunderbird
+        # (nixpkgs-gimp-master.legacyPackages.${pkgs.system}.gimp)
+        obs-studio
+        mission-center
+      ]
+      ++ lib.optionals cfg.desktop.forGaming [ wineWowPackages.stagingFull ]
       ++ lib.optionals cfg.desktop.forDev [
         nodejs
-        corepack
         gcc
         clang-tools
         python3
         httpie
-      ]) ++ cfg.software.extraPackages;
+      ]
+    )
+    ++ cfg.software.extraPackages;
 }
