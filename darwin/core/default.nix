d53405719f58ea42fa0cfc802f3d80d993e41598
Victor B. <contact@itsvic.dev>
2025-12-03 19:30:28 +0100

    this might've broken in the latest nix-darwin too

--- darwin/core/default.nix
+++ darwin/core/default.nix
@@ -1,18 +1,25 @@
-{ pkgs, config, lib, globalSecretsFile, ... }: {
+{
+  pkgs,
+  config,
+  lib,
+  globalSecretsFile,
+  ...
+}:
+{
   imports = [ ../../system/core/nix.nix ];
 
   system.stateVersion = 6;
   system.primaryUser = "vic";
 
-  system.build.applications = lib.mkForce (pkgs.buildEnv {
-    name = "system-applications";
-    paths = config.environment.systemPackages
-      ++ config.home-manager.users.vic.home.packages;
-    pathsToLink = "/Applications";
-  });
+  # system.build.applications = lib.mkForce (pkgs.buildEnv {
+  #   name = "system-applications";
+  #   paths = config.environment.systemPackages
+  #     ++ config.home-manager.users.vic.home.packages;
+  #   pathsToLink = "/Applications";
+  # });
 
   # disable HM's default linking behaviour (does not play well with Launchpad or Spotlight)
-  home-manager.users.vic.targets.darwin.linkApps.enable = false;
+  # home-manager.users.vic.targets.darwin.linkApps.enable = false;
 
   # install ncurses (has better clear :P)
   environment.systemPackages = with pkgs; [ ncurses ];
