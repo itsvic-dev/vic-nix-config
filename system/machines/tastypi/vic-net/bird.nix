cad644ff768ff89118aa46833ddf1da95da046a1
Victor B. <contact@itsvic.dev>
2026-01-24 21:21:31 +0100

    funny

--- system/machines/tastypi/vic-net/bird.nix
+++ system/machines/tastypi/vic-net/bird.nix
@@ -16,6 +16,19 @@
       include "${intranet.birdShared}";
 
       ${intranet.getAllIBGP config}
+
+      protocol bgp isp_funny {
+        enable extended messages on;
+        local as OWNAS;
+        neighbor fe80::1247:aa59:7a3c:f8e0%end0 as 4266660003;
+
+        ipv4 {
+          import where is_valid_network() && !is_self_net();
+          export where is_valid_network() && source ~ [RTS_STATIC, RTS_BGP];
+          import limit 1000 action block;
+          extended next hop on;
+        };
+      }
     '';
   };
 
