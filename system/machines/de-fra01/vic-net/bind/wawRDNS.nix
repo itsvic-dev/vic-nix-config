7eb65b2112af9becc5126553853dff8604a20df3
Victor B. <contact@itsvic.dev>
2026-01-25 22:08:07 +0100

    epic fail

--- system/machines/de-fra01/vic-net/bind/wawRDNS.nix
+++ system/machines/de-fra01/vic-net/bind/wawRDNS.nix
@@ -3,27 +3,29 @@ let
   range =
     first: last: if first > last then [ ] else builtins.genList (n: first + n) (last - first + 1);
 in
-builtins.concatStringsSep "\n" [
-  ''
-    $TTL 24h
-    $ORIGIN 0.32.10.in-addr.arpa.
+builtins.concatStringsSep "\n" (
+  [
+    ''
+      $TTL 24h
+      $ORIGIN 0.32.10.in-addr.arpa.
 
-    @ IN SOA ns1.vic.iw. contact.itsvic.dev. (
-      2026011101  ; serial
-      3h          ; refresh
-      1h          ; retry
-      24h         ; expire
-      60          ; min TTL
-    )
-    @     IN  NS  ns1.vic.iw.
+      @ IN SOA ns1.vic.iw. contact.itsvic.dev. (
+        2026011101  ; serial
+        3h          ; refresh
+        1h          ; retry
+        24h         ; expire
+        60          ; min TTL
+      )
+      @     IN  NS  ns1.vic.iw.
 
-    1     IN  PTR waw.isp.vic.iw.
-  ''
-]
-++ (map (
-  a:
-  let
-    b = toString a;
-  in
-  "${b} IN PTR 10.32.0.${b}.isp.vic.iw."
-) (range 10 200))
+      1     IN  PTR waw.isp.vic.iw.
+    ''
+  ]
+  ++ (map (
+    a:
+    let
+      b = toString a;
+    in
+    "${b} IN PTR 10.32.0.${b}.isp.vic.iw."
+  ) (range 10 200))
+)
