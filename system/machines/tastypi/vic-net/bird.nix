5e492589c07a702620dcf63fab35ba6f3b771fce
Victor B. <contact@itsvic.dev>
2026-01-25 19:38:10 +0100

    Update bird.nix

--- system/machines/tastypi/vic-net/bird.nix
+++ system/machines/tastypi/vic-net/bird.nix
@@ -17,18 +17,18 @@
 
       ${intranet.getAllIBGP config}
 
-      protocol bgp isp_funny {
-        enable extended messages on;
-        local as OWNAS;
-        neighbor fe80::1247:aa59:7a3c:f8e0%end0 as 4266660003;
+      # protocol bgp isp_funny {
+      #   enable extended messages on;
+      #   local as OWNAS;
+      #   neighbor fe80::1247:aa59:7a3c:f8e0%end0 as 4266660003;
 
-        ipv4 {
-          import where is_valid_network() && !is_self_net();
-          export where is_valid_network() && source ~ [RTS_STATIC, RTS_BGP];
-          import limit 1000 action block;
-          extended next hop on;
-        };
-      }
+      #   ipv4 {
+      #     import where is_valid_network() && !is_self_net();
+      #     export where is_valid_network() && source ~ [RTS_STATIC, RTS_BGP];
+      #     import limit 1000 action block;
+      #     extended next hop on;
+      #   };
+      # }
     '';
   };
 
