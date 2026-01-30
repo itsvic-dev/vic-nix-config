1335104e1090c15127eaa628ceb8187024442a68
Victor B. <contact@itsvic.dev>
2026-01-30 16:48:17 +0100

    force masquerade

--- system/machines/pl-waw01/forgejo-runner.nix
+++ system/machines/pl-waw01/forgejo-runner.nix
@@ -16,5 +16,12 @@
     };
   };
 
+  networking.firewall.extraCommands = ''
+    iptables -t nat -A POSTROUTING -s 172.17.0.0/16 -d 10.21.0.0/24 -j MASQUERADE
+  '';
+  networking.firewall.extraStopCommands = ''
+    iptables -t nat -D POSTROUTING -s 172.17.0.0/16 -d 10.21.0.0/24 -j MASQUERADE
+  '';
+
   sops.secrets.forgejo-runner-token = { };
 }
