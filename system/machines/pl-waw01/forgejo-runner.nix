11d6c0a5509d5d93ab8be9be74e90da86dc86293
Victor B. <contact@itsvic.dev>
2026-02-06 00:44:18 +0100

    h

--- system/machines/pl-waw01/forgejo-runner.nix
+++ system/machines/pl-waw01/forgejo-runner.nix
@@ -23,16 +23,12 @@
   # fucking piece of shit child istg
   virtualisation.docker.daemon.settings.iptables = false;
   networking.firewall.extraCommands = ''
-    iptables -t nat -A POSTROUTING -s 172.17.0.0/16 -d 10.21.0.0/24 -j SNAT --to-source 10.21.0.4
-    iptables -t nat -A POSTROUTING -s 172.18.0.0/16 -d 10.21.0.0/24 -j SNAT --to-source 10.21.0.4
-    iptables -t nat -A POSTROUTING -s 172.17.0.0/16 -d 0.0.0.0/0 -j MASQUERADE
-    iptables -t nat -A POSTROUTING -s 172.18.0.0/16 -d 0.0.0.0/0 -j MASQUERADE
+    iptables -t nat -A POSTROUTING -s 172.0.0.0/8 -d 10.21.0.0/24 -j SNAT --to-source 10.21.0.4
+    iptables -t nat -A POSTROUTING -s 172.0.0.0/8 -d 0.0.0.0/0 -j MASQUERADE
   '';
   networking.firewall.extraStopCommands = ''
-    iptables -t nat -D POSTROUTING -s 172.17.0.0/16 -d 10.21.0.0/24 -j SNAT --to-source 10.21.0.4
-    iptables -t nat -D POSTROUTING -s 172.18.0.0/16 -d 10.21.0.0/24 -j SNAT --to-source 10.21.0.4
-    iptables -t nat -D POSTROUTING -s 172.17.0.0/16 -d 0.0.0.0/0 -j MASQUERADE
-    iptables -t nat -D POSTROUTING -s 172.18.0.0/16 -d 0.0.0.0/0 -j MASQUERADE
+    iptables -t nat -D POSTROUTING -s 172.0.0.0/8 -d 10.21.0.0/24 -j SNAT --to-source 10.21.0.4
+    iptables -t nat -D POSTROUTING -s 172.0.0.0/8 -d 0.0.0.0/0 -j MASQUERADE
   '';
 
   sops.secrets.forgejo-runner-token = { };
